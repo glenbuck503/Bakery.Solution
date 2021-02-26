7d2cee7bc03ceff88e71f7cce2e35aa640b2d7df
glenbuck503 <glenbuck503@gmail.com>
2021-02-26 11:05:07 -0800

    good test for BreadOrderPrice_ReturnsBreadTotalPriceIfBuying4WithDeal

--- Bakery.Tests/ModelTests/BakeryTests.cs
+++ Bakery.Tests/ModelTests/BakeryTests.cs
@@ -22,25 +22,25 @@ namespace Bakery.Tests
       Bread newBreadOrder = new Bread(breadOrder);
       int result = newBreadOrder.BreadOrderPrice();
       Assert.AreEqual(5, result);
-     
+
     }
 
     [TestMethod]
-      public void BreadOrderPrice_ReturnsBreadTotalPriceIfBuying2WithDeal()
-      {
-        int breadOrder = 2;
-        Bread newBreadOrder = new Bread(breadOrder);
-        int result = newBreadOrder.BreadOrderPrice();
-        Assert.AreEqual(10, result);
-      }
+    public void BreadOrderPrice_ReturnsBreadTotalPriceIfBuying2WithDeal()
+    {
+      int breadOrder = 2;
+      Bread newBreadOrder = new Bread(breadOrder);
+      int result = newBreadOrder.BreadOrderPrice();
+      Assert.AreEqual(10, result);
+    }
 
-      [TestMethod]
-      public void BreadOrderPrice_ReturnsBreadTotalPriceIfBuying4WithDeal()
-      {
-        int breadOrder = 4;
-        Bread newBreadOrder = new Bread(breadOrder);
-        int result = newBreadOrder.BreadOrderPrice();
-        Assert.AreEqual(10,result);
-      }
+    [TestMethod]
+    public void BreadOrderPrice_ReturnsBreadTotalPriceIfBuying4WithDeal()
+    {
+      int breadOrder = 4;
+      Bread newBreadOrder = new Bread(breadOrder);
+      int result = newBreadOrder.BreadOrderPrice();
+      Assert.AreEqual(20, result);
     }
   }
+}
