2d25560e94f063d05c7e6ea4042589b6a699cdc4
glenbuck503 <glenbuck503@gmail.com>
2021-02-26 14:25:55 -0800

    program works to show total for bread. Including if user enters an amount including the bread deal

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -11,7 +11,13 @@ namespace Bakery.Models
       Console.WriteLine("Pastries are $2 each. PASTRY SPECIAL: Buy 3 for $5!");
       Console.WriteLine("How many loaves of bread would you like? :");
       int custBread = int.Parse(Console.ReadLine());
-      Console.WriteLine(custBread);
+      Console.WriteLine("How many pastries would you like? :");
+      int custPastry = int.Parse(Console.ReadLine());
+
+      Bread breadOrder = new Bread(custBread);
+      Pastry pastryOrder = new Pastry(custPastry);
+      
+      Console.WriteLine($"Your bread order total price is ${breadOrder.BreadOrderPrice()} ");
     }
   }
 }
