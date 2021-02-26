dde260d79bff4a9f14434ec5b015bbe7e273a17b
glenbuck503 <glenbuck503@gmail.com>
2021-02-26 12:21:58 -0800

    create Pastry consructor

--- Bakery/Models/Pastry.cs
+++ Bakery/Models/Pastry.cs
@@ -2,5 +2,14 @@ using System;
 
 namespace Pastry.Models
 {
-  
+  public class Pastry
+  {
+    public int CustomerPastry {get; set;}
+
+    public Pastry(int customerOrder)
+    {
+      CustomerPastry = customerOrder;
+    }
+  }
+
 }
